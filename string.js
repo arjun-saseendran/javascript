61183b752eeda9da80ce7ff689b21c6bb2cb3f2e
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-11 16:28:23 +0530

    add string.js

---
+++ string.js
@@ -0,0 +1,20 @@
+console.log("Hello World\"It's working as expected\"That's good");
+
+const name = "Javascript";
+const name1 = "Javscript";
+const name3 = `Javscript`;
+
+const name4 = new String("Javascript");
+
+const position1 = name.charAt(1);
+
+console.log(position1);
+console.log("Javascript"[1]);
+
+const a = "a";
+const b = "b";
+if (a > b) {
+  console.log("a greaterthan b");
+} else {
+  console.log("b greaterthan a");
+}
