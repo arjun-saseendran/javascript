dbae50c69d3f19dfb7b47e7369dd132f8645a11a
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-13 05:50:08 +0530

    add array.js

---
+++ array.js
@@ -0,0 +1,22 @@
+// Declaring number array
+const nums = [5, 2, 4, 1, 3];
+
+console.log(nums);
+
+// Array with with mutiple data types
+const user = ["Arjun", 34, "Cherthala"];
+
+console.log(user);
+
+// Multidimentional array
+const multiArray = [
+  ["a", "b"],
+  [1, 2],
+  ["c", "d"],
+  [3, 4],
+  ["e", "f"],
+  [5, 6],
+];
+
+console.log(multiArray[0][0]);
+console.log(multiArray[0][1]);
