70ec77f7539077be9629fdd81f5785615def060f
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-14 11:28:29 +0530

    logical operators

---
+++ operators.js
@@ -0,0 +1,50 @@
+
+
+
+// Equality operators
+let num = 10;
+let num1 = '10';
+
+console.log(num == num1);
+console.log(num === num1);
+console.log(num != num1);
+console.log(num !== num1);
+
+// Comparison operators
+let num2 = 5;
+let num3 = 2;
+
+console.log(num2 > num3);
+console.log(num2 >= num3);
+console.log(num2 < num3);
+console.log(num2 <= num3);
+
+// Arithmetic  operators
+let num4 = 3;
+let num5 = 5;
+
+console.log(num4 + num5);
+console.log(num4 - num5);
+console.log(num4 * num5);
+console.log(num4 / num5);
+console.log(num4++);
+console.log(++num5);
+console.log(num4--);
+console.log(--num5);
+console.log(num4 += num5);
+console.log(num4 -= num5);
+console.log(num4 *= num5);
+console.log(num4 /= num5);
+
+// Logical operators
+let name = 'Javascript';
+let title = 'Java';
+
+console.log(name || title);
+console.log(name && title);
+console.log(!name && title);
+
+
+
+
+
